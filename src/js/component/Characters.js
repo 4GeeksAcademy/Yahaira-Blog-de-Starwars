b7e5288384ad950a3add0ba873c6998aae383b78
YahairaCM <63628432+Yahaira326@users.noreply.github.com>
2023-07-24 23:07:22 +0000

    up

--- src/js/component/Characters.js
+++ src/js/component/Characters.js
@@ -1,8 +1,11 @@
 import React from "react";
+//import { useState } from "react";
+//import { Link } from "react-router-dom";
 
- const Characters = () => {
+const Characters = () => {
     return (
         <h1>Characters</h1>
     );
 }
 
+export default Characters;
