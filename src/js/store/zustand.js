36110240744df71236007284c7d0e98f95920044
YahairaCM <63628432+Yahaira326@users.noreply.github.com>
2023-07-25 00:45:34 +0000

    up

---
+++ src/js/store/zustand.js
@@ -0,0 +1,35 @@
+import create from "zustand";
+
+const useStore = create((set, get) => ({
+  people: [],
+  vehicles: [],
+  planets: [],
+  favorite_entity: [],
+  addPeople: (person) => {
+    set({
+      people: get().people.concat(person),
+    });
+  },
+  addVehicle: (vehicle) => {
+    set({
+      vehicles: get().vehicles.concat(vehicle),
+    });
+  },
+  addPlanets: (planet) => {
+    set({
+      planets: get().planets.concat(planet),
+    });
+  },
+  addFavoriteEntity: (name) => {
+    set({
+      favorite_entity: [...new Set(get().favorite_entity.concat(name))]
+    })
+  },
+  updateFavoriteEntity: (targetIndex) => {
+    set({
+      favorite_entity: get().favorite_entity.filter((e, currentIndex) => currentIndex !== targetIndex)
+    })
+  }
+}));
+
+export default useStore;
